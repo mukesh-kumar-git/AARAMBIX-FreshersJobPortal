46dc1679b186cde213abe1a94109c11e39ceba6c
mukeshMike <mukeshkumar.tm17@gmail.com>
2026-01-31 15:44:12 +0530

    Fix Whitenoise static files for production

--- FreshersJobPortal/settings.py
+++ FreshersJobPortal/settings.py
@@ -124,13 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
